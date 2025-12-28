7d1d9cb5d0aff587363a42d7eb39b66069b9c738
amrutha2508 <amrutha.k5637@gmail.com>
2025-12-28 12:21:34 -0600

    Add configuration files

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // output: "export",
+
+  // images: {
+  //   unoptimized: true,
+  // },
+
+  // ⚠️ Replace with your GitHub repo name
+  // basePath: "/html-portfolio",
+  // assetPrefix: "/html-portfolio/",
+};
+
+export default nextConfig;
